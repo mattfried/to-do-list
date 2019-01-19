612d0df8468d96352e6c8bd4c3bcfa93de23a4bf
Matt Fried <mattfried@unknown101f748dc1c5.gateway.2wire.net>
2019-01-19 15:33:25 -0600

    Updates

---
+++ precache-manifest.747dfd0233589a673d7455ccb1e3a1ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4dd0a20738a7e30753a",
+    "url": "/to-do-list/static/css/main.f076783b.chunk.css"
+  },
+  {
+    "revision": "a4dd0a20738a7e30753a",
+    "url": "/to-do-list/static/js/main.a4dd0a20.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/to-do-list/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "af0bf1497ad58103b57b",
+    "url": "/to-do-list/static/js/runtime~main.af0bf149.js"
+  },
+  {
+    "revision": "c316516b29da6be3b7f577ba8e140244",
+    "url": "/to-do-list/index.html"
+  }
+];
